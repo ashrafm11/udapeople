005365e71c9280ddca877349cfeaa1639193ecaa
ashrafm11 <ashrafm11@github.com>
2023-03-26 00:49:18 +0200

    deploy back-end

--- frontend/src/app/components/LoadingMessage/index.tsx
+++ frontend/src/app/components/LoadingMessage/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Component } from 'react';
 import style from './style.local.css';
 
-export namespace LoadingMessage {d23few
+export namespace LoadingMessage {
   export interface Props {
     message: string;
   }
